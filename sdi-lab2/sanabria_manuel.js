f1762e10fc6ce77251263d754916e08b2d6d793f
Manuel Sanabria <mannystevan@gmail.com>
2014-07-10 22:36:43 -0400

    pushing lab 2

---
+++ sdi-lab2/sanabria_manuel.js
@@ -0,0 +1,18 @@
+//alert("JavaScript works!");
+
+// Manuel Sanabria
+// SDI 1407
+// Lab 2: Inputs and Outputs
+
+// Variables
+
+var myNameIs = "Manuel Sanabria";
+var myStreetAddress = "12433 Blacksmith Drive";
+var myFriendsNameIs = "Grace Zotting";
+var myFriendsLives = "Winter Park Villas";
+var myFavoriteSport = "Lacrosse";
+var myTimeToGetHere =  3;
+var myFriendsNameIsGrace = true;
+
+// Outputs 
+console.log("My name is " + myNameIs + ".");
