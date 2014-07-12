e31dc204f7eb6c13235e1dfb6100f2e2937b676d
Manuel Sanabria <mannystevan@gmail.com>
2014-07-12 19:03:17 -0400

    lectureedit5

--- sdi_lab3/sdi-lab3/Sanabria_Manuel.js
+++ sdi_lab3/sdi-lab3/Sanabria_Manuel.js
@@ -5,13 +5,20 @@
 // Leture 3 
 
 var myNameIs = "Manuel Sanabria";
+var myAge = 19
 
 myNameIs = prompt("Enter your name?", "Manuel Sanabria");
 
-if (myNameIs === "Manuel Sanabria") {
+if (myNameIs === "Manuel Sanabria") 
+{
 	console.log("Welcome " + myNameIs + ".");
-
+    if (myAge > 19)
+       console.log("In two years you can legally drink!");   
 } else {
+
+	   console.log("Don't drink!'");
+	   
+	   
 	console.log("Step away from the computer please!");
 }
 
