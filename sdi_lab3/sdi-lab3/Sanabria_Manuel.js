39134c8a11aa65c804b1d9dc77860cb22f1acf10
Manuel Sanabria <mannystevan@gmail.com>
2014-07-12 18:23:59 -0400

    lectureedit4

--- sdi_lab3/sdi-lab3/Sanabria_Manuel.js
+++ sdi_lab3/sdi-lab3/Sanabria_Manuel.js
@@ -11,6 +11,8 @@ myNameIs = prompt("Enter your name?", "Manuel Sanabria");
 if (myNameIs === "Manuel Sanabria") {
 	console.log("Welcome " + myNameIs + ".");
 
+} else {
+	console.log("Step away from the computer please!");
 }
 
 console.log("Good job, Manuel!");
