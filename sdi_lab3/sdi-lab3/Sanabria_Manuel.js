834a981be6324a50b66efa16ab3839c958f76256
Manuel Sanabria <mannystevan@gmail.com>
2014-07-12 18:13:25 -0400

    lectureedit

---
+++ sdi_lab3/sdi-lab3/Sanabria_Manuel.js
@@ -0,0 +1,25 @@
+//alert("JavaScript works!");
+
+// Manuel Sanabria
+// SDI 1407
+// Leture 3 
+
+var myHello = "Hello, world!";
+
+if (myHello === "Hello, world!") {
+	console.log (myHello);
+}
+
+console.log(myGoodbye);
+
+var myNameIs = "Manuel Sanabria";
+
+myNameIs = prompt("Enter your name?", "Manuel Sanabria");
+
+if (myNameIs === "Manuel Sanabria") {
+	console.log("Welcome" + myNameIs + ".");
+
+}
+
+console.log("I did it!");
+
