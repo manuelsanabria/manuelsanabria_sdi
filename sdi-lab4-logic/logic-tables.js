45e9d56385450a7669aa56e7e05baaed94b61ae0
Manuel Sanabria <mannystevan@gmail.com>
2014-07-17 17:15:50 -0400

    labedit

--- sdi-lab4-logic/logic-tables.js
+++ sdi-lab4-logic/logic-tables.js
@@ -5,15 +5,27 @@
 //  Assignment 1
 //  Using logical operators
 
+//chart 
+
 var p;
 var q;
 var r;
 
 p = confirm("First value:  Click OK for true or Cancel for false.");
 q = confirm("Second value:  Click OK for true or Cancel for false.");
+r = confirm("Third Value: Click OK for true or Cancel for false.");
 
 if (p && q) {
-    console.log("With " + p + " and " + q + ", the outcome is TRUE.");
+    console.log("With " + p + " and " + q + "and" + r + ", the outcome is TRUE.");
 } else {
-    console.log("With " + p + " and " + q + ", the outcome is FALSE.");
-};
+    console.log("With " + p + " and " + q + "and" + r + ", the outcome is FALSE.");
+}
+
+// Problem 1
+
+var seniorAge = 55;
+var juniorAge = 10;
+var discount = 7;
+var regualar = 12;
+
+
