bb7875c434311c81a2885f8472d9c04c1744ba96
Manuel Sanabria <mannystevan@gmail.com>
2014-07-17 22:51:56 -0400

    subtractioncommit

--- sdi-lab5/Sanabria_Manuel_Lab5.js
+++ sdi-lab5/Sanabria_Manuel_Lab5.js
@@ -12,18 +12,27 @@ var addition = function(number1, number2) {
 	
 };
 
+var subtract = function(number1, number2) {
+	
+	var subtractingResult;
+	subtractingResult = (number1 - number2);
+	return subtractingResult;
+};	
+
 var a = parseFloat(prompt("Enter your first number, please."));
 var b = parseFloat(prompt("Enter your second number, please."));
 var d = prompt("Enter your operation.");
 if (d === "+") {
 	
 	var myAdd = addition(a, b);
-    console.log(myAdd);
-    
-} else {
+    console.log(myAdd);	
 	
-	console.log("Incorrect");
+} else if (d === "-") {
+
+    var mySubtract = subtract(a, b);
+    console.log(mySubtract);	
 	
-}
+};
+
 
 
