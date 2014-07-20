aad2db5215b58131d930aaa4b1ef57fd0c1f844c
Manuel Sanabria <mannystevan@gmail.com>
2014-07-19 21:42:02 -0400

    lab6-1commit1

---
+++ sdi-lab6/Sanabria_Manuel_Lab6.js
@@ -0,0 +1,18 @@
+//alert("JavaScript works!");
+
+// Manuel Sanabria
+// SDI 1407
+// Lab 6 Part 1
+
+var myList = function() {
+	
+	var list = ["Manchester City", "Manchester United", "Everton", "Chelsea"];
+	var list2 = ["Yaya", "De Gea", "Lukaku", "Mata"]
+	
+	
+	
+	
+	
+	
+};
+
