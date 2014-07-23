8d3bd1668842cb8b8cab4d1088b72efff7e5cd5a
Manuel Sanabria <mannystevan@gmail.com>
2014-07-22 21:44:29 -0400

    firstcommitlab7

---
+++ sdi-lab7/Sanabria_Manuel_Lab7.js
@@ -0,0 +1,19 @@
+//alert("JavaScript works!");
+
+// Manuel Sanabria
+// SDI 1407
+// Lab 7
+
+var firstPlay,
+    secondPlay,
+    thirdPlay;
+    
+var objPlayers = function() {
+	
+	this.player = playName,
+	this.position = playPos,
+	this.nationality = playNat,
+	this.age = playAge;
+	
+	
+};
---
+++ sdi-lab7/artist.js
@@ -0,0 +1,31 @@
+//jsondata
+
+// Manuel Sanabria
+// SDI 1407 
+// Lab 7
+
+var jsonData = {
+	
+	"manchesterUnited":
+	[
+       {
+       	"player": "Juan Mata",
+       	"positon": "Winger",
+       	"nationality": "Spaniard",
+       	"age": 26
+       	
+       },
+       {
+       	"player": "David De Gea",
+       	"position": "Goalkeeper",
+       	"nationality": "Spaniard",
+       	"age": 23
+       },
+       {
+       	"player": "Shinji Kagawa",
+       	"position": "Winger",
+        "nationality": "Japanese",
+        "age": 25
+       }
+     ]
+   };
