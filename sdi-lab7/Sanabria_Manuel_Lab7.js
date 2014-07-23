e22582f76f3dd6a9111cadd3f4ad5c9441581c68
Manuel Sanabria <mannystevan@gmail.com>
2014-07-22 21:53:00 -0400

    thirdcommitlab7

--- sdi-lab7/Sanabria_Manuel_Lab7.js
+++ sdi-lab7/Sanabria_Manuel_Lab7.js
@@ -19,9 +19,21 @@ var objPlayers = function(playName, playPos, playNat, playAge) {
 
 var playerGenerator = function() {
 	
-	
-	
-	
-	
-	
+	for (var index in jsonData.manchesterUnited) {
+		if (index == 0) {
+			
+			firstPlay = new objPerson(jsonData.manchesterUnited[index].player, jsonData.manchesterUnited[index].position, jsonData.manchesterUnited[index].nationality, jsonData.manchesterUnited[index].age);
+			
+		} else if (index == 1) {
+			
+			
+			secondPlay = new objPerson(jsonData.manchesterUnited[index].player, jsonData.manchesterUnited[index].position, jsonData.manchesterUnited[index].nationality, jsonData.manchesterUnited[index].age);
+			
+		} else {
+			
+			thirdPlay = new objPerson(jsonData.manchesterUnited[index].player, jsonData.manchesterUnited[index].position, jsonData.manchesterUnited[index].nationality, jsonData.manchesterUnited[index].age);
+			
+		}
+	};
+
 };
