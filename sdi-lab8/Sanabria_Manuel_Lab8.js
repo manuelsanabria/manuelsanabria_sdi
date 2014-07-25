23bec820a67202112d7ee088bdfd4cf84881c13f
Manuel Sanabria <mannystevan@gmail.com>
2014-07-24 23:56:14 -0400

    commitfinal

--- sdi-lab8/Sanabria_Manuel_Lab8.js
+++ sdi-lab8/Sanabria_Manuel_Lab8.js
@@ -4,18 +4,43 @@ var myEmail = "manuelsanabria@fullsail.edu";
 
 var myChar = function(email) {
 
-	var atSign = myEmail.indexOf("@"); //rich note: atSign = 14
-	//var period = myEmail.indexOf(".");
-	//var atSign2 = myEmail.lastIndexOf("@");
+	var atSign = myEmail.indexOf("@");
+	var period = myEmail.indexOf(".");
+	var atSign2 = myEmail.lastIndexOf("@");
+	var period2 = myEmail.lastIndexOf(".");
+	var space = myEmail.indexOf(" ");
+	var isThereOneAt = false;
+	var isThereOnePeriod = false;
+	var isThereNoSpace = false;
 
-	console.log(atSign);
+	if (atSign >= 0) {
+		if (atSign2 == atSign) {
+			isThereOneAt = true;
+		} else {
+			isThereOneAt = false;
+		}
+	}
 
-	if(atSign > 0) {
-		console.log("Thank you for entering a correct email.");
+	if (period >= 0) {
+		isThereOnePeriod = true;
 	} else {
-		console.log("Please use a @ sign.");
+		isThereOnePeriod = false;
+	}
+
+	if (space == -1) {
+		isThereNoSpace = true;
+	} else {
+		isThereNoSpace = false;
 	}
 	
-};
+	if (isThereOneAt ==  true && isThereOnePeriod == true && isThereNoSpace == true) {
+		console.log("You have a valid address!");
+		return true;	
+	} else {
+		console.log("You have an incorrect address");
+		return false;
+	}
+	
 
+};
 myChar(myEmail);
