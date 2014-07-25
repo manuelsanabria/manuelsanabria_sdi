f849d77b203f5e59b5c66ca799235add0d4e66f8
Manuel Sanabria <mannystevan@gmail.com>
2014-07-24 23:11:24 -0400

    commmit2p1edits

--- sdi-lab8/Sanabria_Manuel_Lab8.js
+++ sdi-lab8/Sanabria_Manuel_Lab8.js
@@ -3,16 +3,19 @@
 var myEmail = "manuelsanabria@fullsail.edu";
 
 var myChar = function(email) {
-	
-	var myIndex = email.charAt(13);
-	
-	if (myIndex == "@") {
-		
-		return true;
-	} else {
-		return false;
-	}
 
+	var atSign = myEmail.indexOf("@"); //rich note: atSign = 14
+	//var period = myEmail.indexOf(".");
+	//var atSign2 = myEmail.lastIndexOf("@");
+
+	console.log(atSign);
+
+	if(atSign > 0) {
+		console.log("Thank you for entering a correct email.");
+	} else {
+		console.log("Please use a @ sign.");
+	}
+	
 };
 
-myChar();
+myChar(myEmail);
