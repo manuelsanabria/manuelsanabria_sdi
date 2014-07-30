2855255322f939cbad55a9b07ecb2f600dfba8dc
Manuel Sanabria <mannystevan@gmail.com>
2014-07-29 23:22:32 -0400

    commit3

--- sdi-lab10/Manuel.js
+++ sdi-lab10/Manuel.js
@@ -4,28 +4,39 @@
 // SDI 1407
 // Lab 10
 
-// global variables 
+// global variables
 var result = 0;
+var array = [];
+var array2 = [];
 
 // functions
-
 var multiples = function() {
-	
-	var sum = 0;
-	for (var i = 3; i < 1000; i+=3) {
-		
-		sum += i;
-		
-	}
-	for (var i = 5; i < 1000; i+=5) {
-		
-		sum += i;
-		
-	}
-return sum;
 
+	var sum = 0;
+	for (var i = 3; i < 1000; i += 3) {
+
+		sum += i;
+
+	}
+	for (var i = 5; i < 1000; i += 5) {
+
+		sum += i;
+
+	}
+	return sum;
+
+};
+var genPowerball = function() {
+	
+	var num = Math.random();
+	num = num * 59;
+	num = parseInt(num);
+	num = num + 1;
+		console.log(num);
 
 };
 result = multiples();
 console.log("The result is " + result + ".");
+genPowerball();
+	
 
